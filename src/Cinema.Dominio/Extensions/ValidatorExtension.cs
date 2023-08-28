2cdfbc6e3adb5c802dd3806b7d36504dd8c6afc6
GuilhermeHDK-DB1 <guilherme.shibukawa@db1.com.br>
2023-08-28 11:03:06 -0300

    alteração no método de adicionar e atualizar Filme para passar pelo ClassificacaoIndicativaExtension

---
+++ src/Cinema.Dominio/Extensions/ValidatorExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace Cinema.Dominio.Extensions
+{
+    public static class ValidatorExtension
+    {
+        public static bool ValidarClassificacaoIndicativa(string classificacao)
+        {
+            if (string.IsNullOrEmpty(classificacao))
+                return false;
+
+
+            return true;
+        }
+    }
+}
