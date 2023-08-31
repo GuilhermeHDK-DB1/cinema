15db4844c68d4c8864b90082728f59d5981cc07e
GuilhermeHDK-DB1 <guilherme.shibukawa@db1.com.br>
2023-08-31 18:20:08 -0300

    validador de ObterSessoesPelaDataQuery

--- src/Cinema.Dominio/Dtos/Sessoes/ObterSessoesPelaDataValidator.cs
+++ src/Cinema.Dominio/Dtos/Sessoes/ObterSessoesPelaDataValidator.cs
@@ -1,4 +1,6 @@
-﻿using FluentValidation;
+﻿using Cinema.Dominio.Common;
+using Cinema.Dominio.Extensions;
+using FluentValidation;
 
 namespace Cinema.Dominio.Dtos.Sessoes
 {
@@ -7,10 +9,8 @@ namespace Cinema.Dominio.Dtos.Sessoes
         public ObterSessoesPelaDataValidator()
         {
             RuleFor(query => query.Data)
-                .NotNull()
-                .NotEmpty()
-                .Matches("^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$")
-                .WithMessage("Data deve seguir o formato AAAA-MM-DD");
+                .Must(data => ValidatorExtension.ValidarData(data))
+                .WithMessage(Resources.FormatoDeDataInvalida);
         }
     }
 }
