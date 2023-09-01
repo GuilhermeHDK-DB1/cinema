940bc4ab2d3569175fbd21260fff6ca7a8fe93f4
GuilhermeHDK-DB1 <guilherme.shibukawa@db1.com.br>
2023-09-01 09:28:13 -0300

    implementado o método de atualizar sessao

--- src/Cinema.Dominio/Common/Resources.cs
+++ src/Cinema.Dominio/Common/Resources.cs
@@ -11,6 +11,7 @@
         public static string GeneroComIdInexistente = "Não existe gênero com este id";
         public static string FilmeComIdInexistente = "Não existe filme com este id";
         public static string SalaComIdInexistente = "Não existe sala com este id";
+        internal static string SessaoComIdInexistente = "Não existe sessao com este id";
 
         public static string GeneroComNomeInexistente = "Gênero do filme informado não existe";
 
---
+++ src/Cinema.Dominio/Dtos/Sessoes/AtualizarSessaoCommand.cs
@@ -0,0 +1,13 @@
+﻿using Cinema.Dominio.Entities.Sessoes;
+
+namespace Cinema.Dominio.Dtos.Sessoes
+{
+    public class AtualizarSessaoCommand
+    {
+        public int Id { get; set; }
+        public int FilmeId { get; set; }
+        public int SalaId { get; set; }
+        public string Horario { get; set; }
+        public Idiomas Idioma { get; set; }
+    }
+}
